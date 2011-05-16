766233325813353ea28ba5ddd4dc07a683779db5
Brett Bieber <brett.bieber@gmail.com>
2011-05-16 11:31:46 -0500

    Update package source URL

--- src/PEAR2/VendorCommands/CLI.php
+++ src/PEAR2/VendorCommands/CLI.php
@@ -10,7 +10,7 @@
  * @copyright 2011 Brett Bieber
  * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
  * @version   SVN: $Id$
- * @link      http://svn.php.net/repository/pear2/PEAR2_VendorCommands
+ * @link      https://github.com/pear2/PEAR2_VendorCommands
  */
 
 /**
@@ -21,7 +21,7 @@
  * @author    Brett Bieber <brett.bieber@gmail.com>
  * @copyright 2011 Brett Bieber
  * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
- * @link      http://svn.php.net/repository/pear2/PEAR2_VendorCommands
+ * @link      https://github.com/pear2/PEAR2_VendorCommands
  */
 namespace PEAR2\VendorCommands;
 class CLI
