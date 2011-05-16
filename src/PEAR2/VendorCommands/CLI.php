dc90359c9c83aae333675f10cd6d1099ea281020
Brett Bieber <brett.bieber@gmail.com>
2011-05-16 11:28:28 -0500

    Add basic functioning vendor-update command

---
+++ src/PEAR2/VendorCommands/CLI.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * PEAR2\VendorCommands\Main
+ *
+ * PHP version 5
+ *
+ * @category  PEAR2
+ * @package   PEAR2_VendorCommands
+ * @author    Brett Bieber <brett.bieber@gmail.com>
+ * @copyright 2011 Brett Bieber
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   SVN: $Id$
+ * @link      http://svn.php.net/repository/pear2/PEAR2_VendorCommands
+ */
+
+/**
+ * Main class for PEAR2_VendorCommands
+ *
+ * @category  PEAR2
+ * @package   PEAR2_VendorCommands
+ * @author    Brett Bieber <brett.bieber@gmail.com>
+ * @copyright 2011 Brett Bieber
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link      http://svn.php.net/repository/pear2/PEAR2_VendorCommands
+ */
+namespace PEAR2\VendorCommands;
+class CLI
+{
+    /**
+     * Command to update dependencies found
+     *
+     * @param PEAR2\Pyrus\ScriptFrontend\Commands $frontend
+     * @param array $args
+     * @param array $options
+     */
+    function updateDependencies($frontend, $args, $options)
+    {
+        $main = new Main();
+
+        if ($args['vendor_dir']) {
+            $main->setPath($args['vendor_dir']);
+        }
+
+        echo 'Scanning '.$main->getPath().' for dependencies...'.PHP_EOL;
+        $deps = $main->scanPackageXMLDependencies();
+
+        echo count($deps) . ' found in the package.xml file'.PHP_EOL;
+
+        // Now run the pyrus frontend with the commands we need
+        $py_args = array('package' => $deps);
+        $py_opts = array(
+                     'force'        => $options['force'],
+                     'optionaldeps' => $options['optionaldeps'],
+                     'plugin'       => false,
+                     'upgrade'      => true,
+        );
+
+        // Now execute the install command for pyrus
+        $frontend->install($py_args, $py_opts);
+
+        echo 'Done'.PHP_EOL;
+    }
+}
---
+++ src/PEAR2/VendorCommands/Main.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * PEAR2\VendorCommands\Main
+ *
+ * PHP version 5
+ *
+ * @category  Yourcategory
+ * @package   PEAR2_VendorCommands
+ * @author    Your Name <handle@php.net>
+ * @copyright 2011 Your Name
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   SVN: $Id$
+ * @link      http://svn.php.net/repository/pear2/PEAR2_VendorCommands
+ */
+
+/**
+ * Main class for PEAR2_VendorCommands
+ *
+ * @category  Yourcategory
+ * @package   PEAR2_VendorCommands
+ * @author    Your Name <handle@php.net>
+ * @copyright 2011 Your Name
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link      http://svn.php.net/repository/pear2/PEAR2_VendorCommands
+ */
+namespace PEAR2\VendorCommands;
+class Main
+{
+    protected static $vendor_dir_name = 'vendor';
+
+    /**
+     * Path to the vendor directory which contains the libs
+     *
+     * @var string
+     */
+    protected $vendor_path;
+
+    /**
+     * The Pyrus config for the vendor registry
+     *
+     * @var PEAR2\Pyrus\Config
+     */
+    protected $config;
+
+    function __construct($path = null)
+    {
+        if (null === $path) {
+            $path = getcwd();
+        }
+
+        $this->setPath($path);
+    }
+
+    /**
+     * Scan for declared dependencies
+     *
+     * @return array
+     */
+    function scanDependencies()
+    {
+        return $this->scanPackageXMLDependencies();
+    }
+
+    /**
+     * Scan for dependencies declared in a package.xml file
+     *
+     * @return array
+     */
+    function scanPackageXMLDependencies()
+    {
+        $deps = array();
+
+        $xml_file = $this->path . DIRECTORY_SEPARATOR . 'package.xml';
+        if (file_exists($xml_file)) {
+            $xml = new \PEAR2\Pyrus\PackageFile($xml_file);
+            foreach ($xml->dependencies['required']->package as $dep) {
+                $dep_description = $dep->channel . '/' . $dep->name;
+                $deps[] = $dep_description;
+            }
+        }
+        return $deps;
+    }
+
+    function scanVendorRegistry()
+    {
+        
+    }
+
+    function installDependencies($deps)
+    {
+        // @TODO
+    }
+
+    function updateDependencies()
+    {
+        // @TODO
+    }
+
+    function setPath($path)
+    {
+        $this->path = $path;
+
+        $this->config = \PEAR2\Pyrus\Config::singleton($path . DIRECTORY_SEPARATOR . self::$vendor_dir_name);
+    }
+
+    function getPath()
+    {
+        return $this->config->path;
+    }
+}
